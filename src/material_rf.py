75e4258ae798bf2872038eb872ff70bcf92d9a6b
kmiringu <75881550+kmiringu@users.noreply.github.com>
2026-08-29 23:26:03 +0300

    chore: load imports for rf classifier

---
+++ src/material_rf.py
@@ -0,0 +1,5 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+import os
